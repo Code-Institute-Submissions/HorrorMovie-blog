ea34b94615df08ee9407da0ced1d762f012bcbc4
Winnypohh <maik523@hotmail.com>
2022-02-01 19:35:58 +0000

    deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://zapzkrrykygfwj:453b038be18e52fe71ac3a63376c6dfd32f40387a31e73a6ed23204f49adf1ad@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d1md939fev1kcq"
+os.environ["SECRET_KEY"] = "Minublog!!1990"
+os.environ["CLOUDINARY_URL"] = "cloudinary://297261645241962:KVsH8L5c46H604LgUMvei_O7EGk@dqgk7izoe"
